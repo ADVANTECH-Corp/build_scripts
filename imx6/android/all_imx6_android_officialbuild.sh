2dc543c14a6588010d7bce3617bae9069fab939b
clayderhua <clayderhua@gmail.com>
2017-03-23 22:49:44 +0800

    update add rom3420 and rom7421

--- imx6/android/all_imx6_android_officialbuild.sh
+++ imx6/android/all_imx6_android_officialbuild.sh
@@ -8,6 +8,12 @@ fi
 if [ $RSB_4411_A1 == true ]; then
 	MACHINE_LIST="$MACHINE_LIST rsb_4411_a1"
 fi
+if [ $ROM_3420_A1 == true ]; then
+	MACHINE_LIST="$MACHINE_LIST rom_3420_a1"
+fi
+if [ $ROM_7421_A1== true ]; then
+	MACHINE_LIST="$MACHINE_LIST rom_7421_a1"
+fi
 
 export MACHINE_LIST
 ./imx6_android_officialbuild.sh

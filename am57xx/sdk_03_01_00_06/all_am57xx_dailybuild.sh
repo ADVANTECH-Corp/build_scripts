b4253bb94ce7f1dcbcf0f602353ce8a04aeab080
alex6235 <alex.cheng@advantech.com.tw>
2017-05-08 17:55:00 +0800

    Add ROM-7510 project to dailybuild script

--- am57xx/sdk_03_01_00_06/all_am57xx_dailybuild.sh
+++ am57xx/sdk_03_01_00_06/all_am57xx_dailybuild.sh
@@ -4,6 +4,9 @@ MACHINE_LIST=""
 if [ $AM57XX_EVM == true ]; then
 	MACHINE_LIST="$MACHINE_LIST am57xx-evm"
 fi
+if [ $ROM7510A1 == true ]; then
+	MACHINE_LIST="$MACHINE_LIST am57xxrom7510a1"
+fi
 
 export MACHINE_LIST
 ./am57xx_dailybuild.sh

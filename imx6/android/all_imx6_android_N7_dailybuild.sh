7f589d1dcad718a22e276f844da525ba0131d0ab
clayderhua <clayderhua@gmail.com>
2017-06-02 17:28:43 +0800

    add Android 7.1.1 dailybuild script for test

---
+++ imx6/android/all_imx6_android_N7_dailybuild.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+MACHINE_LIST=""
+
+# Check build machine name
+if [ $RSB_4410_A1 == true ]; then
+        MACHINE_LIST="$MACHINE_LIST rsb_4410_a1"
+fi
+if [ $RSB_4410_A2 == true ]; then
+	MACHINE_LIST="$MACHINE_LIST rsb_4410_a2"
+fi
+if [ $RSB_4411_A1 == true ]; then
+	MACHINE_LIST="$MACHINE_LIST rsb_4411_a1"
+fi
+if [ $ROM_3420_A1 == true ]; then
+	MACHINE_LIST="$MACHINE_LIST rom_3420_a1"
+fi
+if [ $ROM_7421_A1 == true ]; then
+	MACHINE_LIST="$MACHINE_LIST rom_7421_a1"
+fi
+
+export MACHINE_LIST
+./imx6_android_N7_ailybuild.sh

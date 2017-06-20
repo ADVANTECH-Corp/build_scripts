c959b7fc3e02568c6a2e9c7baffecd63848f38c1
clayderhua <clayderhua@gmail.com>
2017-06-20 09:42:48 +0800

    Update m6.0.1 build script add rsb6410

--- imx6/android/all_imx6_android_officialbuild.sh
+++ imx6/android/all_imx6_android_officialbuild.sh
@@ -17,6 +17,12 @@ fi
 if [ $ROM_7421_A1 == true ]; then
 	MACHINE_LIST="$MACHINE_LIST rom_7421_a1"
 fi
+if [ $RSB_6410_A1 == true ]; then
+        MACHINE_LIST="$MACHINE_LIST rsb_6410_a1"
+fi
+if [ $RSB_6410_A2 == true ]; then
+        MACHINE_LIST="$MACHINE_LIST rsb_6410_a2"
+fi
 
 export MACHINE_LIST
 ./imx6_android_officialbuild.sh

21c7d42cfc007bdc3c98abd1a9b992ba45050697
clayderhua <clayderhua@gmail.com>
2017-08-24 11:15:08 +0800

    update for test

--- imx6/imx6_officialbuild_test.sh
+++ imx6/imx6_officialbuild_test.sh
@@ -561,7 +561,6 @@ function prepare_images()
                 "normal")
                         FILE_NAME=${DEPLOY_IMAGE_NAME}"-"${KERNEL_CPU_TYPE}${PRODUCT}"*.rootfs.sdcard"
                         cp $DEPLOY_IMAGE_PATH/$FILE_NAME $OUTPUT_DIR
-                        cp $DEPLOY_IMAGE_PATH/modules*   $OUTPUT_DIR
                         if [ -e $OUTPUT_DIR/$FILE_NAME ]; then
                                 FILE_NAME=`ls $OUTPUT_DIR | grep rootfs.sdcard | grep $DEPLOY_IMAGE_NAME`
 
@@ -724,17 +723,17 @@ if [ "$PRODUCT" == "$VER_PREFIX" ]; then
 	rm $ROOT_DIR/setup-environment $ROOT_DIR/fsl-setup-release.sh
 	cp $ROOT_DIR/.repo/manifests/fsl-setup-release.sh $ROOT_DIR/fsl-setup-release.sh
 	cp $ROOT_DIR/.repo/manifests/setup-environment $ROOT_DIR/setup-environment
-	#tar czf $ROOT_DIR.tgz $ROOT_DIR --exclude-vcs --exclude .repo
-        #generate_md5 $ROOT_DIR.tgz
+	tar czf $ROOT_DIR.tgz $ROOT_DIR --exclude-vcs --exclude .repo
+        generate_md5 $ROOT_DIR.tgz
 
         # Build Yocto SDK
-        #echo "[ADV] build yocto sdk"
-        #build_yocto_sdk
+        echo "[ADV] build yocto sdk"
+        build_yocto_sdk
 
-	    #echo "[ADV] generate sdk image"
-        #SDK_DIR="$ROOT_DIR"_sdk
-        #prepare_images sdk $SDK_DIR
-	    #copy_image_to_storage sdk
+	    echo "[ADV] generate sdk image"
+        SDK_DIR="$ROOT_DIR"_sdk
+        prepare_images sdk $SDK_DIR
+	    copy_image_to_storage sdk
 
 	# Commit and create meta-advantech branch
 	# create_branch_and_commit $META_ADVANTECH_PATH

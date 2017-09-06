cbda3e379a50584cd1af64a70d84a0950776a6a6
Daniel Hung <daniel.hung@advantech.com.tw>
2017-09-06 13:54:07 +0800

    [Qcom] Exit with correct return code
    To send correct return code back to Jenkins, we cannot gather log files here.
    But, it's fine because we still can see the log message on Jenkins.

--- db410c/oe-rpb/all_410c_officialbuild.sh
+++ db410c/oe-rpb/all_410c_officialbuild.sh
@@ -5,13 +5,16 @@ NUM2=`expr $VERSION : '.*[.]\([0-9]*\)'`
 VERSION_NUM=$NUM1$NUM2
 
 #410c_BSP
-./410c_oe_officialbuild.sh 410c 410cLBV"$VERSION_NUM" 2>&1 |tee 410cLBV"$VERSION_NUM"_$DATE.log
+./410c_oe_officialbuild.sh 410c 410cLBV"$VERSION_NUM" 2>&1
+[ "$?" -ne 0 ] && exit 1
+
 #410c_projects
 if [ $RSB_4760 == true ]; then
-	./410c_oe_officialbuild.sh rsb-4760 4760LIV"$VERSION_NUM" 2>&1 |tee 4760LIV"$VERSION_NUM"_$DATE.log
+	./410c_oe_officialbuild.sh rsb-4760 4760LIV"$VERSION_NUM" 2>&1
+	[ "$?" -ne 0 ] && exit 1
 fi
 if [ $EPC_R4761 == true ]; then
-	./410c_oe_officialbuild.sh epc-r4761 4761LIV"$VERSION_NUM" 2>&1 |tee 4761LIV"$VERSION_NUM"_$DATE.log
+	./410c_oe_officialbuild.sh epc-r4761 4761LIV"$VERSION_NUM" 2>&1
+	[ "$?" -ne 0 ] && exit 1
 fi
 
-mv *.log $STORED/$DATE/

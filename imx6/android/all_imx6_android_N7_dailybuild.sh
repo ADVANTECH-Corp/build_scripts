a4d589f0a48be235c7965192fa4f02103e0312db
Clayder.Huang <clayder.huang@advantech.com.tw>
2018-05-22 22:13:30 +0800

    for debug android_n7

--- imx6/android/all_imx6_android_N7_dailybuild.sh
+++ imx6/android/all_imx6_android_N7_dailybuild.sh
@@ -2,23 +2,21 @@
 MACHINE_LIST=""
 
 # Check build machine name
-#if [ $RSB_4410_A1 == true ]; then
+#if [ $RSB4410A1 == true ]; then
 #        MACHINE_LIST="$MACHINE_LIST rsb_4410_a1"
 #fi
-if [ $RSB_4411_A1 == true ]; then
+if [ $RSB4411A1 == true ]; then
 	MACHINE_LIST="$MACHINE_LIST rsb_4411_a1"
 fi
-if [ $UBC_220_A1 == true ]; then
-
-export UBC_220_A1
+if [ $UBC220A1 == true ]; then
 	MACHINE_LIST="$MACHINE_LIST ubc_220_a1"
 fi
-#if [ $ROM_3420_A1 == true ]; then
+#if [ $ROM3420A1 == true ]; then
 #	MACHINE_LIST="$MACHINE_LIST rom_3420_a1"
 #fi
-#if [ $ROM_7421_A1 == true ]; then
+#if [ $ROM7421A1 == true ]; then
 #	MACHINE_LIST="$MACHINE_LIST rom_7421_a1"
 #fi
 
 export MACHINE_LIST
-#./imx6_android_N7_dailybuild.sh
+./imx6_android_N7_dailybuild.sh

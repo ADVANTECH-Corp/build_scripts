864f14293faee578b5e5990c22ec49e11ac00f63
Clayder.Huang <clayder.huang@advantech.com.tw>
2018-05-24 22:36:57 +0800

    update android n7 officialbuild

--- imx6/android/all_imx6_android_N7_officialbuild.sh
+++ imx6/android/all_imx6_android_N7_officialbuild.sh
@@ -2,30 +2,20 @@
 MACHINE_LIST=""
 
 # Check build machine name
-if [ $RSB_4410_A1 == true ]; then
-        MACHINE_LIST="$MACHINE_LIST rsb_4410_a1"
-fi
-if [ $RSB_4410_A2 == true ]; then
-	MACHINE_LIST="$MACHINE_LIST rsb_4410_a2"
-fi
-if [ $RSB_4411_A1 == true ]; then
+#if [ $RSB4410A1 == true ]; then
+#        MACHINE_LIST="$MACHINE_LIST rsb_4410_a1"
+#fi
+if [ $RSB4411A1 == true ]; then
 	MACHINE_LIST="$MACHINE_LIST rsb_4411_a1"
 fi
-if [ $ROM_3420_A1 == true ]; then
-	MACHINE_LIST="$MACHINE_LIST rom_3420_a1"
+if [ $UBC220A1 == true ]; then
+	MACHINE_LIST="$MACHINE_LIST ubc_220_a1"
 fi
-if [ $ROM_7421_A1 == true ]; then
-	MACHINE_LIST="$MACHINE_LIST rom_7421_a1"
-fi
-if [ $RSB_6410_A1 == true ]; then
-        MACHINE_LIST="$MACHINE_LIST rsb_6410_a1"
-fi
-if [ $RSB_6410_A2 == true ]; then
-        MACHINE_LIST="$MACHINE_LIST rsb_6410_a2"
-fi
-if [ $ROM_5420_B1 == true ]; then
-        MACHINE_LIST="$MACHINE_LIST rom_5420_b1"
-fi
-
+#if [ $ROM3420A1 == true ]; then
+#	MACHINE_LIST="$MACHINE_LIST rom_3420_a1"
+#fi
+#if [ $ROM7421A1 == true ]; then
+#	MACHINE_LIST="$MACHINE_LIST rom_7421_a1"
+#fi
 export MACHINE_LIST
-./imx6_android_officialbuild.sh
+./imx6_android_N7_officialbuild.sh

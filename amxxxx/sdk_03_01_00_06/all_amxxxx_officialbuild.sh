628f59fcfc1020620c610531565a331059f8ce22
zhengguang18282 <zhengguang.yang@advantech.com.cn>
2019-08-25 20:48:45 +0800

    [TI] add EPC-R3220A1

--- amxxxx/sdk_03_01_00_06/all_amxxxx_officialbuild.sh
+++ amxxxx/sdk_03_01_00_06/all_amxxxx_officialbuild.sh
@@ -24,6 +24,9 @@ fi
 if [ "$ROM3310A1" == "true" ]; then
 	MACHINE_LIST="$MACHINE_LIST am335xrom3310a1"
 fi
+if [ "$EPCR3220A1" == "true" ]; then
+	MACHINE_LIST="$MACHINE_LIST am335xepcr3220a1"
+fi
 
 export MACHINE_LIST
 ./amxxxx_officialbuild.sh $VERSION_NUM

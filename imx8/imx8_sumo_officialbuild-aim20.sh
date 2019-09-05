338a85de07c603aa074cfb6d9432bab340612583
Daniel Hung <daniel.hung@advantech.com.tw>
2019-09-05 10:58:55 +0800

    [imx] Generate SDK with populate_sdk

--- imx8/imx8_sumo_officialbuild-aim20.sh
+++ imx8/imx8_sumo_officialbuild-aim20.sh
@@ -315,9 +315,9 @@ function building()
         LOG_DIR="$OFFICIAL_VER"_"$CPU_TYPE"_"$DATE"_log
 
         if [ "$1" == "populate_sdk" ]; then
-                if [ "$DEPLOY_IMAGE_NAME" == "fsl-image-validation-imx" ]; then
-                        echo "[ADV] bitbake meta-toolchain"
-                        bitbake meta-toolchain
+                if [ "$DEPLOY_IMAGE_NAME" == "fsl-image-qt5" ]; then
+                        echo "[ADV] bitbake meta-toolchain-qt5"
+                        bitbake meta-toolchain-qt5
                 else
                         echo "[ADV] bitbake $DEPLOY_IMAGE_NAME -c populate_sdk"
                         bitbake $DEPLOY_IMAGE_NAME -c populate_sdk

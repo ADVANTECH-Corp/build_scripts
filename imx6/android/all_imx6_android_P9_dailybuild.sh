bb282dba5d4475cfd4ac93f75b04650b783be32c
liszt.kao <liszt.kao@advantech.com.tw>
2019-10-25 10:47:58 +0800

    imx6 android9 daily build.

---
+++ imx6/android/all_imx6_android_P9_dailybuild.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+MACHINE_LIST=""
+
+# Check build machine name
+#if [ $RSB4410A1 == true ]; then
+#        MACHINE_LIST="$MACHINE_LIST rsb_4410_a1"
+#fi
+if [ $RSB4411A1 == true ]; then
+	MACHINE_LIST="$MACHINE_LIST rsb_4411_a1"
+fi
+
+export MACHINE_LIST
+./imx6_android_P9_dailybuild.sh

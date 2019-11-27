6bb63a703d8c0cf2b1dfd7bd852e255de199cbb2
clayderhua <clayderhua@gmail.com>
2019-11-27 10:41:10 +0800

    Add DS211 project in dailybuild and officialbuild

--- rockchip/all_rk_android_N7_officialbuild.sh
+++ rockchip/all_rk_android_N7_officialbuild.sh
@@ -23,4 +23,15 @@ if [ "$DS100L" == "true" ]; then
 	./rk_android_N7_officialbuild.sh $VERSION_NUM
 	[ "$?" -ne 0 ] && exit 1
 fi
+#DS211_projects
+if [ "$DS211" == "true" ]; then
+        KERNEL_DTB=rk3399-ds211.img
+        KERNEL_CONFIG=rockchip_ds211_defconfig
+        MACHINE_LIST="$MACHINE_LIST ds211"
+        export KERNEL_DTB
+        export KERNEL_CONFIG
+        export MACHINE_LIST
+        ./rk_android_N7_officialbuild.sh $VERSION_NUM
+        [ "$?" -ne 0 ] && exit 1
+fi
 echo "[ADV] All done!"

a5b325ff13601d081de6f8afb2f63a986a7c6b14
zhengguang18282 <zhengguang.yang@advantech.com.cn>
2020-04-09 13:46:54 +0800

    [TI] Ubuntu script support EPC-R3220A1

--- amxxxx/sdk_05_03_00_07/amxxxx_package_ubuntu.sh
+++ amxxxx/sdk_05_03_00_07/amxxxx_package_ubuntu.sh
@@ -193,6 +193,10 @@ fi
 if [ "$ROM3310A1" == "true" ]; then
         MACHINE_LIST="$MACHINE_LIST am335xrom3310a1"
 fi
+if [ "$EPCR3220A1" == "true" ]; then
+        MACHINE_LIST="$MACHINE_LIST am335xepcr3220a1"
+fi
+
 
 NUM1=`expr $VERSION : 'V\([0-9]*\)'`
 NUM2=`expr $VERSION : '.*[.]\([0-9A-Z]*\)'`

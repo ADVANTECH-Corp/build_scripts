1242c11ed46f695e6c6a611d4e047a68dcbf501c
yunjin.jiang@advantech.com.cn <yunjin.jiang@advantech.com.cn>
2020-04-10 12:30:57 +0800

    add debian9 rb04

--- rockchip/rk3288_debian9_rb04_officialbuild.sh
+++ rockchip/rk3288_debian9_rb04_officialbuild.sh
@@ -278,7 +278,7 @@ function building()
 		sudo dpkg -i ubuntu-build-service/packages/*
 		sudo apt-get install -f 
 		cd $CURR_PATH/$ROOT_DIR/
-		./mk-debian.sh TRUE >> $CURR_PATH/$ROOT_DIR/$LOG_FILE_ROOTFS
+		./mk-debian.sh >> $CURR_PATH/$ROOT_DIR/$LOG_FILE_ROOTFS
 
 	else
         echo "[ADV] pass building..."

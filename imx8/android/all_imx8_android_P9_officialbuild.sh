51691b3ff01d2d0a983b23485c03e18a10b73809
wei.zeng <wei.zeng@advantech.com.cn>
2020-09-14 15:34:16 +0800

    added imx8 p9 scripts

---
+++ imx8/android/all_imx8_android_P9_officialbuild.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+MACHINE_LIST=""
+SOC_NAME=""
+if  [ $ROM5720A1 == true ]; then
+	MACHINE_LIST="$MACHINE_LIST rom5720_a1"
+	SOC_NAME="imx8mq"
+fi
+
+export MACHINE_LIST
+export SOC_NAME
+./imx8_android_P9_officialbuild.sh

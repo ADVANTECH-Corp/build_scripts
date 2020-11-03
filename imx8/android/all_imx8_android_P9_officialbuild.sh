0e4c46b454455d245ffdf520ebc961cd2da12fa8
wei.zeng <wei.zeng@advantech.com.cn>
2020-11-03 09:32:04 +0800

    added ROM5721 and ROM7720

--- imx8/android/all_imx8_android_P9_officialbuild.sh
+++ imx8/android/all_imx8_android_P9_officialbuild.sh
@@ -6,6 +6,16 @@ if  [ $ROM5720A1 == true ]; then
 	SOC_NAME="imx8mq"
 fi
 
+if  [ $ROM5721A1 == true ]; then
+        MACHINE_LIST="$MACHINE_LIST rom5721_a1"
+        SOC_NAME="imx8mm"
+fi
+
+if  [ $ROM7720A1 == true ]; then
+        MACHINE_LIST="$MACHINE_LIST rom7720_a1"
+        SOC_NAME="imx8qm"
+fi
+
 export MACHINE_LIST
 export SOC_NAME
 ./imx8_android_P9_officialbuild.sh

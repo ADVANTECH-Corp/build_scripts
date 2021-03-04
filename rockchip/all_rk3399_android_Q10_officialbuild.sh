8709ede8d02d32fba3a1eddcb6194c54c6bbd0b0
wei.zeng <wei.zeng@advantech.com.cn>
2021-03-04 11:22:56 +0800

    support rk3399 android10

---
+++ rockchip/all_rk3399_android_Q10_officialbuild.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+NUM1=`expr $VERSION : 'V\([0-9]*\)'`
+NUM2=`expr $VERSION : '.*[.]\([0-9A-Z]*\)'`
+VERSION_NUM=$NUM1$NUM2
+
+MACHINE_LIST=""
+
+#rk3399_projects
+if [ "$RSB4710A2" == "true" ]; then
+	UBOOT_DEFCONFIG=rk3399_rsb4710a2_2G
+	KERNEL_DEFCONFIG=rk3399_adv_defconfig
+	KERNEL_DTB=rk3399-rsb4710-a2.img
+	ANDROID_PRODUCT=rsb4710_Android10-userdebug
+	MACHINE_LIST="$MACHINE_LIST rsb_4710"
+
+	export UBOOT_DEFCONFIG
+	export KERNEL_DEFCONFIG
+	export KERNEL_DTB
+	export ANDROID_PRODUCT
+	export MACHINE_LIST
+	./rk3399_android_Q10_officialbuild.sh $VERSION_NUM
+fi
+
+if [ "$ROM5780A3" == "true" ]; then
+	UBOOT_DEFCONFIG=rk3399_rom5780a3_2G
+	KERNEL_DEFCONFIG=rk3399_adv_defconfig
+	KERNEL_DTB=rk3399-rom5780-a3.img
+	ANDROID_PRODUCT=rom5780_Android10-userdebug
+	MACHINE_LIST="$MACHINE_LIST rom_5780"
+
+	export UBOOT_DEFCONFIG
+	export KERNEL_DEFCONFIG
+	export KERNEL_DTB
+	export ANDROID_PRODUCT
+	export MACHINE_LIST
+	./rk3399_android_Q10_officialbuild.sh $VERSION_NUM
+fi

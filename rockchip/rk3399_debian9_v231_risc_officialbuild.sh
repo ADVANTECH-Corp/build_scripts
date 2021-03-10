76c645d333ac16c1eabdf7e62c1da62f25717d79
advyunjin <yunjin.jiang@advantech.com.cn>
2021-03-10 13:47:31 +0800

    cp RKDevTool or Android to image for RK3399

--- rockchip/rk3399_debian9_v231_risc_officialbuild.sh
+++ rockchip/rk3399_debian9_v231_risc_officialbuild.sh
@@ -329,10 +329,16 @@ function prepare_images()
     IMAGE_DIR="${OFFICIAL_VER}"_"$DATE"
     echo "[ADV] mkdir $IMAGE_DIR"
     mkdir $IMAGE_DIR
-	mkdir -p $IMAGE_DIR/rockdev/image
+
+    if [ -d $CURR_PATH/$ROOT_DIR/tools/windows/RKDevTool ];then
+        cp -aRL $CURR_PATH/$ROOT_DIR/tools/windows/RKDevTool/* $IMAGE_DIR/
+    else
+        cp -aRL $CURR_PATH/$ROOT_DIR/tools/windows/AndroidTool/* $IMAGE_DIR/
+    fi
+
+    mkdir -p $IMAGE_DIR/rockdev/image
 
     # Copy image files to image directory
-
     cp -aRL $CURR_PATH/$ROOT_DIR/rockdev/* $IMAGE_DIR/rockdev/image
     echo "[ADV] creating ${IMAGE_DIR}.tgz ..."
     tar czf ${IMAGE_DIR}.img.tgz $IMAGE_DIR

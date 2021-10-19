923b92365708d0160a627539003bda9a79d40b90
Darren Huang <darren.huang@advantech.com.tw>
2021-10-19 09:28:44 +0800

    Fix official build fail

--- imx8/imx8_zeus_officialbuild-aim30.sh
+++ imx8/imx8_zeus_officialbuild-aim30.sh
@@ -311,6 +311,16 @@ function save_temp_log()
 	find . -name "temp" | xargs rm -rf
 }
 
+function get_bsp_tarball()
+{
+	if [ -e $STORAGE_PATH/${ROOT_DIR}.tgz ] ; then
+		tar zxf $STORAGE_PATH/${ROOT_DIR}.tgz
+	else
+		echo "[ADV] Cannot find BSP tarball"
+		exit 1;
+	fi
+}
+
 function get_csv_info()
 {
 	IMAGE_DIR="$OFFICIAL_VER"_"$CPU_TYPE"_"$1"_"$DATE"
@@ -336,31 +346,42 @@ function get_csv_info()
 # ================
 define_cpu_type $PRODUCT
 
-mkdir $ROOT_DIR
-get_source_code
+if [ "$PRODUCT" == "$VER_PREFIX" ]; then
+	echo "[ADV] get bsp tarball"
+	get_bsp_tarball
 
-if [ -z "$EXISTED_VERSION" ] ; then
-	# Get info from CSV
-	for MEMORY in $MEMORY_LIST;do
-		get_csv_info $MEMORY
-	done
-	# Check meta-advantech tag exist or not, and checkout to tag version
-	check_tag_and_checkout $META_ADVANTECH_PATH $META_ADVANTECH_BRANCH $HASH_ADVANTECH
+	rm -rf $ROOT_DIR
 
-	# Check tag exist or not, and replace bbappend file SRCREV
-	check_tag_and_replace $U_BOOT_PATH $U_BOOT_URL $HASH_UBOOT
-	check_tag_and_replace $KERNEL_PATH $KERNEL_URL $HASH_KERNEL
+else # "$PRODUCT" != "$VER_PREFIX"
+	mkdir $ROOT_DIR
+	get_source_code
 
-	commit_tag_and_rollback $META_ADVANTECH_PATH
+        if [ -z "$EXISTED_VERSION" ] ; then
+		# Get info from CSV
+		for MEMORY in $MEMORY_LIST;do
+			get_csv_info $MEMORY
+		done
+		# Check meta-advantech tag exist or not, and checkout to tag version
+		check_tag_and_checkout $META_ADVANTECH_PATH $META_ADVANTECH_BRANCH $HASH_ADVANTECH
 
-	# Add git tag and Package kernel & u-boot
-	echo "[ADV] Add tag"
-	commit_tag_and_package $U_BOOT_URL $U_BOOT_BRANCH $HASH_UBOOT
-	commit_tag_and_package $KERNEL_URL $KERNEL_BRANCH $HASH_KERNEL
+		# Check tag exist or not, and replace bbappend file SRCREV
+		check_tag_and_replace $U_BOOT_PATH $U_BOOT_URL $HASH_UBOOT
+		check_tag_and_replace $KERNEL_PATH $KERNEL_URL $HASH_KERNEL
 
-	# Create manifests xml and commit
-	create_xml_and_commit
+                commit_tag_and_rollback $META_ADVANTECH_PATH
+
+                # Add git tag and Package kernel & u-boot
+                echo "[ADV] Add tag"
+                commit_tag_and_package $U_BOOT_URL $U_BOOT_BRANCH $HASH_UBOOT
+                commit_tag_and_package $KERNEL_URL $KERNEL_BRANCH $HASH_KERNEL
+
+                # Create manifests xml and commit
+                create_xml_and_commit
+        fi
 fi
+
+#cd $CURR_PATH
+#rm -rf $ROOT_DIR
 
 echo "[ADV] build script done!"
 

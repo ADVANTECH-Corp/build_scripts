a51132689b928694fd1be77c3823288456fbdbaa
advyunjin <yunjin.jiang@advantech.com.cn>
2021-12-13 16:11:21 +0800

    add project RSB3710A2 for all_rk3399_android_Q10_officialbuild

--- rockchip/all_rk3399_android_Q10_officialbuild.sh
+++ rockchip/all_rk3399_android_Q10_officialbuild.sh
@@ -21,6 +21,21 @@ if [ "$RSB4710A2" == "true" ]; then
 	./rk3399_android_Q10_officialbuild.sh $VERSION_NUM
 fi
 
+if [ "$RSB3710A2" == "true" ]; then
+	UBOOT_DEFCONFIG=rk3399_rsb3710a2_2G
+	KERNEL_DEFCONFIG=rk3399_adv_defconfig
+	KERNEL_DTB=rk3399-rsb3710-a2.img
+	ANDROID_PRODUCT=rsb3710_Android10-userdebug
+	MACHINE_LIST="$MACHINE_LIST rsb_3710"
+
+	export UBOOT_DEFCONFIG
+	export KERNEL_DEFCONFIG
+	export KERNEL_DTB
+	export ANDROID_PRODUCT
+	export MACHINE_LIST
+	./rk3399_android_Q10_officialbuild.sh $VERSION_NUM
+fi
+
 if [ "$ROM5780A3" == "true" ]; then
 	UBOOT_DEFCONFIG=rk3399_rom5780a3_2G
 	KERNEL_DEFCONFIG=rk3399_adv_defconfig

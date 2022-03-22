a6d7e8f69c0f8f217ddbdaa995c847234a582919
advyunjin <yunjin.jiang@advantech.com.cn>
2022-03-22 15:20:02 +0800

    add project rk3568_debian10_v110_risc

---
+++ rockchip/all_rk3568_debian10_v110_risc_officialbuild.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# NUM1=`expr $VERSION : 'V\([0-9]*\)'`
+# NUM2=`expr $VERSION : '.*[.]\([0-9A-Z]*\)'`
+# VERSION_NUM=$NUM1$NUM2
+
+VERSION_NUM=${RELEASE_VERSION}
+
+MACHINE_LIST=""
+
+#rk3568_projects
+
+if [ "$RSB4810A2" == "true" ]; then
+	UBOOT_DEFCONFIG=rk3568_rsb4810a2
+	KERNEL_DEFCONFIG=rk3568_adv_defconfig
+	KERNEL_DTB=rk3568-rsb4810-a2.img
+	MACHINE_LIST="$MACHINE_LIST RSB4810"
+	MODEL_NAME="RSB4810"
+	HW_VER="A2"
+
+	export UBOOT_DEFCONFIG
+	export KERNEL_DEFCONFIG
+	export KERNEL_DTB
+	export MACHINE_LIST
+	export MODEL_NAME
+	export HW_VER
+	./rk3568_debian10_v110_risc_officialbuild.sh $VERSION_NUM
+fi
+
---
+++ rockchip/azure_build_rk3568_debian10_v110_risc.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+BUILD_PATH=$1
+GIT_TOKEN=$2
+
+echo "-------------------------------------------------"
+echo "Build in $0 begin ..."
+echo "BUILD_PATH:	${BUILD_PATH}"
+echo "GIT_TOKEN :	${GIT_TOKEN}"
+DATE_TIME=`date +%Y%m%d%H%M%S`
+
+# bring up docker container
+DOCK_ID=$(sudo docker run -itd --name build_rk3568_linux_risc_${DATE_TIME} -v ${BUILD_PATH}:/home/adv/BSP:rw --privileged advrisc/u18.04-rklbv1:latest /bin/bash)
+
+
+# config git in docker container
+sudo docker exec ${DOCK_ID} /bin/bash -c "
+git config --global credential.helper 'store --file ~/.my-credentials'; \
+echo "${GIT_TOKEN}" > ~/.my-credentials; \
+git config --global user.name "advrisc"; \
+git config --global user.email "advrisc@gmail.com" "
+
+# build in docker container
+sudo docker exec ${DOCK_ID} /bin/bash -c "
+sudo chmod a+rw /home/adv/BSP/ -R; \
+cd /home/adv/BSP; \
+soure azure_env.sh; \
+./all_rk3568_debian10_v110_risc_officialbuild.sh"
+
+# stop & rm docker container
+sudo docker stop ${DOCK_ID}
+sudo docker rm ${DOCK_ID}
+
+echo "Build in $0 end"
+echo "-------------------------------------------------"
+
+

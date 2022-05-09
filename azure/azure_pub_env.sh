c0b3ec9afce221dde5112f3a06f6225e1d7992d9
advyunjin <yunjin.jiang@advantech.com.cn>
2022-05-09 15:50:17 +0800

    add azure_pub_env.sh

---
+++ azure/azure_pub_env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export PUB_BLOB_DAILY_BUILD_ROOT="https://riscsw.blob.core.windows.net/dailybuild"
+export PUB_BLOB_DAILY_BUILD_ROOT_TOKEN="sp=racwdl&st=2022-03-29T02:14:44Z&se=2030-03-31T10:14:44Z&spr=https&sv=2020-08-04&sr=c&sig=va5zmrnJ1l8JCx2qU9F09OpltEl15ud26nOL76jIUAg%3D"
+export PUB_BLOB_OFFICIAL_BUILD_ROOT="https://riscsw.blob.core.windows.net/officialbuild"
+export PUB_BLOB_OFFICIAL_BUILD_ROOT_TOKEN="sp=racwdl&st=2022-03-29T02:16:19Z&se=2030-03-31T10:16:19Z&spr=https&sv=2020-08-04&sr=c&sig=LH%2B1YuJwXRxrWMlqRkpKP0B3kDSjVv%2BbD2WrnH28Uyw%3D"
+
+export PUB_GIT_AZURE_TOKEN="https://AIM-Linux:gjnbqzmnszapmkxpngqud65nwddkevmn2okjgaeiyc2bgrjhn3pa@dev.azure.com"
+export PUB_GIT_GITHUB_TOKEN=""
+
+

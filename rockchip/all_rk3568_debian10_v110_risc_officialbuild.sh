1fb1b03d42b76dcd93944834329f700c39934975
advyunjin <yunjin.jiang@advantech.com.cn>
2022-08-15 13:51:19 +0800

    add project ROM-5880 A1

--- rockchip/all_rk3568_debian10_v110_risc_officialbuild.sh
+++ rockchip/all_rk3568_debian10_v110_risc_officialbuild.sh
@@ -27,3 +27,20 @@ if [ "$RSB4810A2" == "true" ]; then
 	./rk3568_debian10_v110_risc_officialbuild.sh $VERSION_NUM
 fi
 
+if [ "$ROM5880A1" == "true" ]; then
+	UBOOT_DEFCONFIG=rk3568_rom5880a1
+	KERNEL_DEFCONFIG=rk3568_adv_defconfig
+	KERNEL_DTB=rk3568-rom5880-a1.img
+	MACHINE_LIST="$MACHINE_LIST ROM5880"
+	MODEL_NAME="ROM5880"
+	HW_VER="A1"
+
+	export UBOOT_DEFCONFIG
+	export KERNEL_DEFCONFIG
+	export KERNEL_DTB
+	export MACHINE_LIST
+	export MODEL_NAME
+	export HW_VER
+	./rk3568_debian10_v110_risc_officialbuild.sh $VERSION_NUM
+fi
+

af5fa152fb8e8d4d0aa170836825798774cff6a3
advrisc <advrisc@gmail.com>
2022-12-13 17:03:31 +0800

    add all_imx8_desktop_hardknott_build-aim33.sh

---
+++ imx8/all_imx8_desktop_hardknott_build-aim33.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+B=${0##*/}
+B=${B/all_}
+
+if [[ ! ${ALSO_BUILD_OFFICIAL_IMAGE} =~ 1|true ]]; then
+	# Dailybuild
+	BUILD_SH=${B/_build/_dailybuild}
+	VERSION_NUM=${RELEASE_VERSION}
+else
+	# Official release
+    [[ ! $VERSION =~ ^V[0-9A-Z][0-9]{4}$ ]] && { echo "invalid VERSION ($VERSION)"; exit 1; }
+	BUILD_SH=${B/_build/_officialbuild}
+	VERSION_NUM=${VERSION#V}
+fi
+echo BUILD_SH=$BUILD_SH
+echo VERSION_NUM=$VERSION_NUM
+
+#imx8_BSP
+echo $BUILD_SH imx8 imx8UBV"$VERSION_NUM" 1G
+[ "$?" -ne 0 ] && exit 1
+
+#imx8_projects
+if [ $RSB3720A1 == true ]; then
+	echo $BUILD_SH rsb3720a1-8MP 3720A1"$AIM_VERSION"UIV"$VERSION_NUM" "6G" "FSPI"
+	[ "$?" -ne 0 ] && exit 1
+fi
+
+echo "[ADV] All done!"

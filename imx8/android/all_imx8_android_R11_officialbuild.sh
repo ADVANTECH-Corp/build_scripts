016cdf141b5c142b16552578de7a97994aa06017
wei.zeng <wei.zeng@advantech.com.cn>
2023-01-17 17:20:31 +0800

    imx8 android R11 init

---
+++ imx8/android/all_imx8_android_R11_officialbuild.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+MACHINE_LIST=""
+SOC_NAME=""
+if  [ $ROM5722A1 == true ]; then
+	MACHINE_LIST="$MACHINE_LIST rom5722_a1"
+	SOC_NAME="imx8mp"
+fi
+
+if  [ $RSB3720A1 == true ]; then
+        MACHINE_LIST="$MACHINE_LIST rsb3720_a1"
+        SOC_NAME="imx8mp"
+fi
+
+export MACHINE_LIST
+export SOC_NAME
+./imx8_android_R11_officialbuild.sh

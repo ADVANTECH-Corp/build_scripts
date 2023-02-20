4cce7eaae15d79199177fbbd9adf60991ef06e7f
advyunjin <yunjin.jiang@advantech.com.cn>
2023-02-20 16:51:27 +0800

    remove FSPI for RSB3730A2

--- imx8/all_imx8_azure_zeus_dailybuild-aim30.sh
+++ imx8/all_imx8_azure_zeus_dailybuild-aim30.sh
@@ -37,7 +37,7 @@ function build_project()
 
 	if [ $RSB3730A2 == true ]; then
 			export BUILD_NN_IMX_CLEANSTATE=false
-		    $BUILD_SH rsb3730a2-8MM 3730A2"$AIM_VERSION"LIV"$VERSION_NUM" "4G 2G" "FSPI"
+		    $BUILD_SH rsb3730a2-8MM 3730A2"$AIM_VERSION"LIV"$VERSION_NUM" "4G 2G" ""
 		    [ "$?" -ne 0 ] && exit 1
 	fi
 

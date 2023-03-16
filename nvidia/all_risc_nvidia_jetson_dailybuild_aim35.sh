33ab9e4a4638a3aa9b43932a939830f3a33e229b
Darren Huang <darren.huang@advantech.com.tw>
2023-03-16 10:34:48 +0800

    [nvidia] Add the aim35 dailybuild script

---
+++ nvidia/all_risc_nvidia_jetson_dailybuild_aim35.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#EPCR7300A1_projects
+if [ "$EPCR7300A1" == "true" ]; then
+    IMAGE_TYPE="external"
+    MODEL_NAME="7300"
+    BOARD_VER="A1"
+    export IMAGE_TYPE
+    export MODEL_NAME
+    export BOARD_VER
+    ./risc_nvidia_jetson_dailybuild_aim35.sh EPCR7300A1
+fi
+
+echo "[ADV] All done!"

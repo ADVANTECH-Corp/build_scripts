a80dd20fca7de032582731894fc4f9abf8a6a3d1
lisztkao <liszt.kao@advantech.com.tw>
2023-03-27 12:02:07 +0800

    [AIR-030]Add AIR-030 project.

--- nvidia/all_risc_nvidia_jetson_dailybuild_aim35.sh
+++ nvidia/all_risc_nvidia_jetson_dailybuild_aim35.sh
@@ -11,4 +11,15 @@ if [ "$EPCR7300A1" == "true" ]; then
     ./risc_nvidia_jetson_dailybuild_aim35.sh EPCR7300A1
 fi
 
+#AIR030A1_projects
+if [ "$AIR030A1" == "true" ]; then
+    IMAGE_TYPE="inner"
+    MODEL_NAME="AIR030"
+    BOARD_VER="A1"
+    export IMAGE_TYPE
+    export MODEL_NAME
+    export BOARD_VER
+    ./risc_nvidia_jetson_dailybuild_aim35.sh AIR030A1
+fi
+
 echo "[ADV] All done!"

e7a1c7c89495dfcdcfd7fc1ce809cbd782afbd06
clayderhua <clayderhua@gmail.com>
2023-04-11 09:54:44 +0800

    [AIM34]Rollback Commit ab801ea and Add 8ulp in Yocto4.0 AIM34

--- imx8/all_imx8_kirkstone_dailybuild-aim34.sh
+++ imx8/all_imx8_kirkstone_dailybuild-aim34.sh
@@ -13,6 +13,10 @@ if [ $ROM5721A1 == true ]; then
 	$BUILD_SH rom5721a1-8MM 5721A1"$AIM_VERSION"LIV"$VERSION_NUM" "2G 1G" ""
 	[ "$?" -ne 0 ] && exit 1
 fi
+if [ $ROM2620A1 == true ]; then
+	$BUILD_SH rom2620a1-8U 2620A1"$AIM_VERSION"LIV"$VERSION_NUM" "2G" ""
+	[ "$?" -ne 0 ] && exit 1
+fi
 if [ $LPDDR4EVK8U == true ]; then
         $BUILD_SH -lpddr4-evk-8U EVK"$AIM_VERSION"LIV"$VERSION_NUM" "2G" ""
         [ "$?" -ne 0 ] && exit 1

5299b10850f4376f946c2a559e3c12629e7b7fd0
Darren Huang <darren.huang@advantech.com.tw>
2023-05-16 18:08:16 +0800

    [nvidia] Add the aim35 officialbuild script

---
+++ nvidia/all_risc_nvidia_jetson_officialbuild_aim35.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+VER_PREFIX="nv"
+export VER_PREFIX
+
+#EPCR7300A1_projects
+if [ "$EPCR7300A1" == "true" ]; then
+    MODEL_NAME="7300"
+    BOARD_VER="A1"
+    TARGET_BOARD="jetson-orin-epcr7300-a1"
+    PROJECT_BRANCH="epcr7300"
+    export MODEL_NAME
+    export BOARD_VER
+    export TARGET_BOARD
+    export PROJECT_BRANCH
+    ./risc_nvidia_jetson_officialbuild_aim35.sh EPCR7300A1
+fi
+
+echo "[ADV] All done!"

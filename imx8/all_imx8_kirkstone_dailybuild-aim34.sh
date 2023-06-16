2bb860beef310596d168b0c17c8b418ec91d4029
lisztkao <liszt.kao@advantech.com.tw>
2023-06-16 13:10:28 +0800

    [DMS-SA66]Add AIM4.0 daily build.

--- imx8/all_imx8_kirkstone_dailybuild-aim34.sh
+++ imx8/all_imx8_kirkstone_dailybuild-aim34.sh
@@ -29,5 +29,8 @@ if [ $LPDDR4EVK8U == true ]; then
         $BUILD_SH -lpddr4-evk-8U EVK"$AIM_VERSION"LIV"$VERSION_NUM" "2G" ""
         [ "$?" -ne 0 ] && exit 1
 fi
-
+if [ $DMSSA66A1 == true ]; then
+        $BUILD_SH imx8mpdmssa66a1-8MP imx8mpdmssa66a1"$AIM_VERSION"LIV"$VERSION_NUM" "6G" ""
+        [ "$?" -ne 0 ] && exit 1
+fi
 echo "[ADV] All done!"

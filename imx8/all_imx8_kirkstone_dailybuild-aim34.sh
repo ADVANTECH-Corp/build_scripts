0109e643c8053d36889b099a3d0cf8270aac5fc6
Darren Huang <darren.huang@advantech.com.tw>
2023-07-28 16:32:37 +0800

    [Yocto4.0][ROM-5722] Add 4G DDR and FSPI to dailybuild

--- imx8/all_imx8_kirkstone_dailybuild-aim34.sh
+++ imx8/all_imx8_kirkstone_dailybuild-aim34.sh
@@ -14,7 +14,7 @@ if [ $ROM5721A1 == true ]; then
 	[ "$?" -ne 0 ] && exit 1
 fi
 if [ $ROM5722A1 == true ]; then
-        $BUILD_SH rom5722a1-8MP 5722A1"$AIM_VERSION"LIV"$VERSION_NUM" "6G" ""
+        $BUILD_SH rom5722a1-8MP 5722A1"$AIM_VERSION"LIV"$VERSION_NUM" "6G 4G" "FSPI"
         [ "$?" -ne 0 ] && exit 1
 fi
 if [ $RSB3720A1 == true ]; then

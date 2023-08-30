3c6fbf8d68f97b7c276fe3cdeebb45526527983d
Hank.Hsueh <Hank.Hsueh@advantech.com.tw>
2023-08-30 15:31:28 +0800

    [IMX93][Yocto4.2]Add imx93 dailybuild scripts

--- imx8/all_imx8_mickledore_dailybuild-aim36.sh
+++ imx8/all_imx8_mickledore_dailybuild-aim36.sh
@@ -13,6 +13,10 @@ if [ $ROM2620A1 == true ]; then
 	$BUILD_SH rom2620a1-8U 2620A1"$AIM_VERSION"LIV"$VERSION_NUM" "2G 1G" ""
 	[ "$?" -ne 0 ] && exit 1
 fi
+if [ $ROM2820A1 == true ]; then
+	$BUILD_SH rom2820a1-93 2820A1"$AIM_VERSION"LIV"$VERSION_NUM" "2G 1G" ""
+	[ "$?" -ne 0 ] && exit 1
+fi
 if [ $LPDDR4EVK8U == true ]; then
         $BUILD_SH -lpddr4-evk-8U EVK"$AIM_VERSION"LIV"$VERSION_NUM" "2G" ""
         [ "$?" -ne 0 ] && exit 1

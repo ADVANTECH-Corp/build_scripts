1c060df9da48e10a441982b9b26e197abc2aa75a
Darren Huang <darren.huang@advantech.com.tw>
2023-10-25 10:46:37 +0800

    [IMX8][Yocto4.2] Add the officialbuild scripts

---
+++ imx8/all_imx8_mickledore_officialbuild-aim36.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "[ADV] Officialbuild start"
+NUM1=`expr $VERSION : 'V\([0-9A-Z]*\)'`
+NUM2=`expr $VERSION : '.*[.]\([0-9A-Z]*\)'`
+# Official release
+BUILD_SH="./imx8_mickledore_officialbuild-aim36.sh"
+VERSION_NUM=$NUM1$NUM2
+
+#imx8_BSP
+$BUILD_SH imx8 imx8LBV"$VERSION_NUM" 1G
+[ "$?" -ne 0 ] && exit 1
+
+#imx8_projects
+if [ $RSB3720A2 == true ]; then
+        $BUILD_SH rsb3720a2-8MP 3720A2"$AIM_VERSION"LIV"$VERSION_NUM" "6G" "FSPI"
+        [ "$?" -ne 0 ] && exit 1
+fi
+
+echo "[ADV] All done!"

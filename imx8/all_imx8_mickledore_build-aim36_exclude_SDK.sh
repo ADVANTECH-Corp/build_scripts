e0e200274ff7240cdf487ee0634342fb10886496
Darren Huang <darren.huang@advantech.com.tw>
2023-10-27 10:20:33 +0800

    [IMX8][Yocto4.2] Add the Yocto 4.2 exclude SDK version officialbuild

---
+++ imx8/all_imx8_mickledore_build-aim36_exclude_SDK.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "[ADV] Officialbuild start"
+NUM1=`expr $VERSION : 'V\([0-9A-Z]*\)'`
+NUM2=`expr $VERSION : '.*[.]\([0-9A-Z]*\)'`
+# Official release
+BUILD_SH="./imx8_mickledore_officialbuild-aim36.sh"
+VERSION_NUM=$NUM1$NUM2
+
+#imx8_projects
+if [ $RSB3720A2 == true ]; then
+        $BUILD_SH rsb3720a2-8MP 3720A2"$AIM_VERSION"LIV"$VERSION_NUM" "6G" "FSPI"
+        [ "$?" -ne 0 ] && exit 1
+fi
+
+echo "[ADV] All done!"

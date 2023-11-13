47e3e1220719e7993fbf1294ddfe04db3d65a1c6
raysu1027 <110970753+raysu1027@users.noreply.github.com>
2023-11-13 16:53:37 +0800

    Update all_risc_nvidia_jetson_dailybuild_aim35.sh
    Add EPCR7200_projects

--- nvidia/all_risc_nvidia_jetson_dailybuild_aim35.sh
+++ nvidia/all_risc_nvidia_jetson_dailybuild_aim35.sh
@@ -11,6 +11,17 @@ if [ "$EPCR7300A1" == "true" ]; then
     ./risc_nvidia_jetson_dailybuild_aim35.sh EPCR7300A1
 fi
 
+#EPCR7200A1_projects
+if [ "$EPCR7200A1" == "true" ]; then
+    IMAGE_TYPE="external"
+    MODEL_NAME="7200"
+    BOARD_VER="A1"
+    export IMAGE_TYPE
+    export MODEL_NAME
+    export BOARD_VER
+    ./risc_nvidia_jetson_dailybuild_aim35.sh EPCR7200A1
+fi
+
 #AIR030A1_projects
 if [ "$AIR030A1" == "true" ]; then
     IMAGE_TYPE="external"

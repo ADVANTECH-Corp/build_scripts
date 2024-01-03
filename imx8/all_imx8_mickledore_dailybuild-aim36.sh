ed9e7bfc33d90a0d582a8bc77aad9a92a78922a6
clayderhua <clayderhua@gmail.com>
2024-01-03 18:46:16 +0800

    [ROM-5722]Add rom57226G dailybuild and officialbuild in Yocto4.2 6.1.22

--- imx8/all_imx8_mickledore_dailybuild-aim36.sh
+++ imx8/all_imx8_mickledore_dailybuild-aim36.sh
@@ -21,4 +21,8 @@ if [ $RSB3720A2 == true ]; then
         $BUILD_SH rsb3720a2-8MP 3720A2"$AIM_VERSION"LIV"$VERSION_NUM" "6G" "FSPI"
         [ "$?" -ne 0 ] && exit 1
 fi
+if [ $ROM5722A1 == true ]; then
+        $BUILD_SH rom5722a1-8MP 5722A1"$AIM_VERSION"LIV"$VERSION_NUM" "6G" "FSPI"
+        [ "$?" -ne 0 ] && exit 1
+fi
 echo "[ADV] All done!"

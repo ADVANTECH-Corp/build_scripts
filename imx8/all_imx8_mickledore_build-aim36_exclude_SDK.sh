0bccb314138cee7f78f3eda2e2fc404841e86de1
SeanChuang2 <dinner0828@gmail.com>
2024-01-22 16:54:43 +0800

    [ROM-2620] Add rom2620 officialbuild in Yocto4.2 6.1.22

--- imx8/all_imx8_mickledore_build-aim36_exclude_SDK.sh
+++ imx8/all_imx8_mickledore_build-aim36_exclude_SDK.sh
@@ -8,6 +8,10 @@ BUILD_SH="./imx8_mickledore_officialbuild-aim36.sh"
 VERSION_NUM=$NUM1$NUM2
 
 #imx8_projects
+if [ $ROM2620A1 == true ]; then
+	$BUILD_SH rom2620a1-8U 2620A1"$AIM_VERSION"LIV"$VERSION_NUM" "2G 1G" ""
+	[ "$?" -ne 0 ] && exit 1
+fi
 if [ $RSB3720A2 == true ]; then
         $BUILD_SH rsb3720a2-8MP 3720A2"$AIM_VERSION"LIV"$VERSION_NUM" "6G" "FSPI"
         [ "$?" -ne 0 ] && exit 1

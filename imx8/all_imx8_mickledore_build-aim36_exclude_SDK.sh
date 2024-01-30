59e0d7e212d60f7eda215e0bf59e68ff80c51a43
clayderhua <clayderhua@gmail.com>
2024-01-30 13:59:39 +0800

    [Yocto4.2]Add ROM-5722 and ROM-5721 in dailybuild and officialbuild

--- imx8/all_imx8_mickledore_build-aim36_exclude_SDK.sh
+++ imx8/all_imx8_mickledore_build-aim36_exclude_SDK.sh
@@ -16,5 +16,12 @@ if [ $RSB3720A2 == true ]; then
         $BUILD_SH rsb3720a2-8MP 3720A2"$AIM_VERSION"LIV"$VERSION_NUM" "6G" "FSPI"
         [ "$?" -ne 0 ] && exit 1
 fi
-
+if [ $ROM5722A1 == true ]; then
+        $BUILD_SH rom5722a1-8MP 5722A1"$AIM_VERSION"LIV"$VERSION_NUM" "6G" "FSPI"
+        [ "$?" -ne 0 ] && exit 1
+fi
+if [ $ROM5721A1 == true ]; then
+	$BUILD_SH rom5721a1-8MM 5721A1"$AIM_VERSION"LIV"$VERSION_NUM" "2G 1G" ""
+	[ "$?" -ne 0 ] && exit 1
+fi
 echo "[ADV] All done!"

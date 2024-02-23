46adca484d94e03631d00c30c15158ac7fe29fd7
kuihong.xin <kuihong.xin@advantech.com.cn>
2024-02-23 13:07:46 +0800

    [rs08] yocto4 all build shell

---
+++ imx8/all_imx8_RS08_kirkstone_build-aim34.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+function build_project()
+{
+	echo "[ADV] $1 start"
+	if [ $1 == "OfficialBuild" ]; then
+		NUM1=`expr $VERSION : 'V\([0-9A-Z]*\)'`
+		NUM2=`expr $VERSION : '.*[.]\([0-9A-Z]*\)'`
+		# Official release
+		BUILD_SH="./imx8_kirkstone_officialbuild-aim34.sh"
+		VERSION_NUM=$NUM1$NUM2
+
+	else
+		# Dailybuild
+		BUILD_SH="./imx8_kirkstone_dailybuild-aim34.sh"
+		VERSION_NUM=${RELEASE_VERSION}
+	fi
+
+	#imx8_BSP
+	if [ $1 == "DailyBuild" ]; then
+		$BUILD_SH imx8 imx8LBV"$VERSION_NUM" 1G
+		[ "$?" -ne 0 ] && exit 1
+	fi
+
+	#imx8_projects
+	if [ $EBCRS08A2 == true ]; then
+		$BUILD_SH ebcrs08a2-8MM RS08A2"$AIM_VERSION"LIV"$VERSION_NUM" "2G" ""
+		[ "$?" -ne 0 ] && exit 1
+	fi
+}
+
+build_project DailyBuild
+
+if [ "${ALSO_BUILD_OFFICIAL_IMAGE}" == true ]; then
+#	export VERSION=VA.`echo $VERSION_NUM | cut -b 2-`
+	build_project OfficialBuild
+fi
+
+echo "[ADV] All done!"

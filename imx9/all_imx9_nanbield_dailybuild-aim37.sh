a2f7a6fffbb9a0c8ea903bb5c8e2a68f1f6950db
SeanChuang2 <dinner0828@gmail.com>
2024-05-30 11:11:10 +0800

    [AIM37][Yocto4.3] Add daily/official build scripts

---
+++ imx9/all_imx9_nanbield_dailybuild-aim37.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+	# Dailybuild
+	echo "[ADV] Dailybuild start"
+	BUILD_SH="./imx9_nanbield_dailybuild-aim37.sh"
+	VERSION_NUM=${RELEASE_VERSION}
+
+#imx9_BSP
+$BUILD_SH imx9 imx9LBV"$VERSION_NUM" 1G
+[ "$?" -ne 0 ] && exit 1
+
+#imx9_projects
+if [ $AOM3511A1 == true ]; then
+	$BUILD_SH aom3511a1-95 3511A1"$AIM_VERSION"LIV"$VERSION_NUM" "16G" ""
+	[ "$?" -ne 0 ] && exit 1
+fi
+echo "[ADV] All done!"

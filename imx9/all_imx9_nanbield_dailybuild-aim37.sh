8c9bb8e60548854726b7ba3b3830c9f630636c7c
SeanChuang2 <dinner0828@gmail.com>
2024-08-21 17:10:08 +0800

    [Yocto4.3][AOM-3511] Add 8G DDR

--- imx9/all_imx9_nanbield_dailybuild-aim37.sh
+++ imx9/all_imx9_nanbield_dailybuild-aim37.sh
@@ -10,7 +10,7 @@ $BUILD_SH imx9 imx9LBV"$VERSION_NUM" 1G
 
 #imx9_projects
 if [ $AOM3511A1 == true ]; then
-	$BUILD_SH aom3511a1-95 3511A1"$AIM_VERSION"LIV"$VERSION_NUM" "16G" ""
+	$BUILD_SH aom3511a1-95 3511A1"$AIM_VERSION"LIV"$VERSION_NUM" "8G 16G" ""
 	[ "$?" -ne 0 ] && exit 1
 fi
 echo "[ADV] All done!"

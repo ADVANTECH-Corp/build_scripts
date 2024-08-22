a51cc4f450287517cdea825928ce4647f25c62aa
SeanChuang2 <dinner0828@gmail.com>
2024-08-22 12:56:52 +0800

    [AFE-E320] Add i.MX93 afee320 official build to Yocto4.2 6.1.36

--- imx9/all_imx9_mickledore_build-aim36_exclude_SDK.sh
+++ imx9/all_imx9_mickledore_build-aim36_exclude_SDK.sh
@@ -12,5 +12,9 @@ if [ $ROM2820A1 == true ]; then
         $BUILD_SH rom2820a1-93 2820A1"$AIM_VERSION"LIV"$VERSION_NUM" "2G" ""
         [ "$?" -ne 0 ] && exit 1
 fi
+if [ $AFEE320A1 == true ]; then
+	$BUILD_SH afee320a1-93 E320A1"$AIM_VERSION"LIV"$VERSION_NUM" "2G" ""
+	[ "$?" -ne 0 ] && exit 1
+fi
 
 echo "[ADV] All done!"

e62353f996bca6b9bc5e2670bc6c2c2c7f6f9525
Hank.Hsueh <Hank.Hsueh@advantech.com.tw>
2024-09-27 18:53:59 +0800

    [imx6_Yocto33] Add new dailybuild Support imx6q rom7420a2

---
+++ imx6/all_imx6_hardknott_build-aim40_new.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Dailybuild
+        echo "[ADV] Dailybuild start"
+        BUILD_SH="./imx6_hardknott_dailybuild-aim40_new.sh"
+        VERSION_NUM=${RELEASE_VERSION}
+
+#imx6_BSP
+$BUILD_SH imx6 imx6LBV"$VERSION_NUM" 1G
+[ "$?" -ne 0 ] && exit 1
+
+#imx6_projects
+if [ $RSB4411A1 == true ]; then
+        $BUILD_SH rsb4411a1-6q 4411A1"$AIM_VERSION"LIV"$VERSION_NUM" "1G" ""
+        [ "$?" -ne 0 ] && exit 1
+fi
+
+if [ $RSB4411A2_SOLO == true ]; then
+        $BUILD_SH rsb4411a2-6dl 4411A2"$AIM_VERSION"LIV"$VERSION_NUM" "2G" ""
+        [ "$?" -ne 0 ] && exit 1
+fi
+
+if [ $RSB3430A1 == true ]; then
+        $BUILD_SH rsb3430a1-6q 3430A1"$AIM_VERSION"LIV"$VERSION_NUM" "1G" ""
+        [ "$?" -ne 0 ] && exit 1
+fi
+
+if [ $ROM7420A2 == true ]; then
+        $BUILD_SH rom7420a2-6q 7420A2"$AIM_VERSION"LIV"$VERSION_NUM" "1G" ""
+        [ "$?" -ne 0 ] && exit 1
+fi
+echo "[ADV] All done!"
+

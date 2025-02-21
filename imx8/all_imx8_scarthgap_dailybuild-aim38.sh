577cded60a3b885f65bb3b9583ee60c1c886e068
Hank.Hsueh <Hank.Hsueh@advantech.com.tw>
2025-02-21 16:39:03 +0800

    [Yocto5.0][ROM-2620] Add rom2620 in project dailybuild.

--- imx8/all_imx8_scarthgap_dailybuild-aim38.sh
+++ imx8/all_imx8_scarthgap_dailybuild-aim38.sh
@@ -23,4 +23,8 @@ if [ $RSB3720A2 == true ]; then
         [ "$?" -ne 0 ] && exit 1
 fi
 
+if [ $ROM2620A1 == true ]; then
+	$BUILD_SH rom2620a1-8U 2620A1"$AIM_VERSION"LIV"$VERSION_NUM" "2G 1G" ""
+	[ "$?" -ne 0 ] && exit 1
+fi
 echo "[ADV] All done!"

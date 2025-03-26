97d835a7137d769168ce832ada005026d66ba058
Winston.Huang <winston.huang@advantech.com.tw>
2025-03-26 13:45:59 +0800

    [AIM34][Yocto4.0][IMX8][RSB-3720A1] modify to create 4GB only

--- imx8/all_imx8_kirkstone_dailybuild-aim34.sh
+++ imx8/all_imx8_kirkstone_dailybuild-aim34.sh
@@ -18,7 +18,7 @@ if [ $ROM5722A1 == true ]; then
         [ "$?" -ne 0 ] && exit 1
 fi
 if [ $RSB3720A1 == true ]; then
-        $BUILD_SH rsb3720a1-8MP 3720A1"$AIM_VERSION"LIV"$VERSION_NUM" "6G 2G" "FSPI"
+        $BUILD_SH rsb3720a1-8MP 3720A1"$AIM_VERSION"LIV"$VERSION_NUM" "4G" "FSPI"
         [ "$?" -ne 0 ] && exit 1
 fi
 if [ $ROM2620A1 == true ]; then

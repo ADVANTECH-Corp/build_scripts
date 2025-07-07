1014ad48477e44a62801f221304b853a2eeccee4
raysu1027 <110970753+raysu1027@users.noreply.github.com>
2025-07-07 17:46:28 +0800

    Update all_risc_nvidia_officialbuild.sh for air020r

--- nvidia/all_risc_nvidia_officialbuild.sh
+++ nvidia/all_risc_nvidia_officialbuild.sh
@@ -14,4 +14,11 @@ if [ "$afer750a1" == "true" ]; then
     ./risc_nvidia_officialbuild.sh
 fi
 
+#AIR020RA1_projects
+if [ "$air020ra1" == "true" ]; then
+    PROJECT_BRANCH="air020r"
+    export PROJECT_BRANCH
+    ./risc_nvidia_officialbuild.sh
+fi
+
 echo "[ADV] All done!"

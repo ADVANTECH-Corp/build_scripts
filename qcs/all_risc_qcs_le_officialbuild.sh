d801d498f78b7f3c64a6eae29c03713f161e944a
liszt.kao <liszt.kao@advantech.com.tw>
2025-07-11 08:12:26 +0800

    [DS-011]Add official build.

--- qcs/all_risc_qcs_le_officialbuild.sh
+++ qcs/all_risc_qcs_le_officialbuild.sh
@@ -5,4 +5,9 @@ if [ "$aom2721a1" == "true" ]; then
     ./risc_qcs_le_officialbuild.sh
 fi
 
+#DS011A1 Project
+if [ "$ds011a1" == "true" ]; then
+    ./risc_qcs_le_officialbuild.sh
+fi
+
 echo "[ADV] All done!"

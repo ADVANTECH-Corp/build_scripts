026bbf73654943959e31ac1053c63e9d896af008
raysu1027 <110970753+raysu1027@users.noreply.github.com>
2025-08-14 16:58:03 +0800

    add ebcrc04 , add ds015, add air021

--- nvidia/all_risc_nvidia_dailybuild.sh
+++ nvidia/all_risc_nvidia_dailybuild.sh
@@ -15,4 +15,19 @@ if [ "$air020ra1" == "true" ]; then
     ./risc_nvidia_dailybuild.sh
 fi
 
+#EBCRC04A1_projects
+if [ "$ebcrc04a1" == "true" ]; then
+    ./risc_nvidia_dailybuild.sh
+fi
+
+#DS015A1_projects
+if [ "$ds015a1" == "true" ]; then
+    ./risc_nvidia_dailybuild.sh
+fi
+
+#AIR021A1_projects
+if [ "$air021a1" == "true" ]; then
+    ./risc_nvidia_dailybuild.sh
+fi
+
 echo "[ADV] All done!"

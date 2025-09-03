4aef8ae356b16e2ce0e87b02d481d2dbe33f3072
Darren Huang <darren.huang@advantech.com.tw>
2025-09-03 16:42:46 +0800

    [QCS6490] Change the checking version method of uname to /etc/OEMInfo.ini

--- qcs/risc_qcs_le_dailybuild.sh
+++ qcs/risc_qcs_le_dailybuild.sh
@@ -7,64 +7,23 @@ echo "[ADV] BSP_URL = ${BSP_URL}"
 echo "[ADV] BSP_BRANCH = ${BSP_BRANCH}"
 echo "[ADV] BSP_XML = ${BSP_XML}"
 echo "[ADV] PLATFORM_PREFIX = ${PLATFORM_PREFIX}"
-echo "[ADV] TARGET_BOARD=$TARGET_BOARD"
 echo "[ADV] PROJECT=$PROJECT"
-echo "[ADV] OS_BSP=$OS_BSP"
-echo "[ADV] DISTRO=$DISTRO"
+echo "[ADV] OS_DISTRO=$OS_DISTRO"
 echo "[ADV] KERNEL_VERSION=$KERNEL_VERSION"
 echo "[ADV] CHIP_NAME=$CHIP_NAME"
 echo "[ADV] RAM_SIZE=$RAM_SIZE"
 echo "[ADV] STORAGE=$STORAGE"
+echo "[ADV] RELEASE_VERSION=$RELEASE_VERSION"
 echo "[ADV] KERNEL_PATH = ${KERNEL_PATH}"
 echo "[ADV] YOCTO_MACHINE_NAME=$YOCTO_MACHINE_NAME"
 echo "[ADV] DISTRO_IMAGE = ${DISTRO_IMAGE}"
 
-if [ ${#PROJECT} -ne 9 ]; then
-    echo "${PROJECT} project is not 9 characters"
-    exit 1
-fi
-
-if [ ${#OS_BSP} -ne 1 ]; then
-    echo "${OS_BSP} os bsp is not 1 character"
-    exit 1
-fi
-
-if [ ${#DISTRO} -ne 4 ]; then
-    echo "${DISTRO} distro is not 4 characters"
-    exit 1
-fi
-
-if [ ${#KERNEL_VERSION} -ne 8 ]; then
-    echo "${KERNEL_VERSION} kernel version is not 8 characters"
-    exit 1
-fi
-
-if [ ${#CHIP_NAME} -ne 5 ]; then
-    echo "${CHIP_NAME} chip name is not 5 characters"
-    exit 1
-fi
-
-if [ ${#RAM_SIZE} -ne 3 ]; then
-    echo "${RAM_SIZE} ram size is not 3 characters"
-    exit 1
-fi
-
-if [ ${#STORAGE} -ne 4 ]; then
-    echo "${STORAGE} storage is not 4 characters"
-    exit 1
-fi
-
-if [ ${#DATE} -ne 10 ]; then
-    echo "${DATE} date is not 10 characters"
-    exit 1
-fi
-
 CURR_PATH="$PWD"
-ROOT_DIR="${PLATFORM_PREFIX}_${PROJECT}_${DATE}"
+ROOT_DIR="${PLATFORM_PREFIX}_${PROJECT}_v${RELEASE_VERSION}_${DATE}"
 OUTPUT_DIR="${CURR_PATH}/${STORED}/${DATE}"
-IMAGE_VER=""
-UFS_IMAGE_VER=""
-EMMC_IMAGE_VER=""
+IMAGE_VER="${PROJECT}_${OS_DISTRO}_v${RELEASE_VERSION}_${KERNEL_VERSION}_${CHIP_NAME}_${RAM_SIZE}_${DATE}"
+UFS_IMAGE_VER="${PROJECT}_${OS_DISTRO}_v${RELEASE_VERSION}_${KERNEL_VERSION}_${CHIP_NAME}_${RAM_SIZE}_${STORAGE}_${DATE}"
+EMMC_IMAGE_VER="${PROJECT}_${OS_DISTRO}_v${RELEASE_VERSION}_${KERNEL_VERSION}_${CHIP_NAME}_${RAM_SIZE}_emmc_${DATE}"
 
 # QIMP
 #YOCTO_IMAGE_DIR="$CURR_PATH/$ROOT_DIR/build-qcom-wayland/tmp-glibc/deploy/images/${YOCTO_MACHINE_NAME}"
@@ -86,36 +45,25 @@ function get_source_code()
 	popd
 }
 
-function get_release_version_and_set_image_version()
+function update_oeminfo()
 {
-	pushd $CURR_PATH/$ROOT_DIR/.repo/manifests 2>&1 > /dev/null
-	RELEASE_VERSION="01"
+    local ini_file="$ROOT_DIR/layers/meta-advantech/recipes-products/images/files/rootfs/etc/OEMInfo.ini"
 
-	for file in *; do
-		if echo "$file" | grep -q "${PROJECT}_${OS_BSP}${DISTRO}"; then
-			RELEASE_VERSION=$(($(echo "ibase=10; $(echo "$file" | cut -c16-17)" | bc) + 1))
-			if ((RELEASE_VERSION >= 2 && RELEASE_VERSION <= 9)); then
-				RELEASE_VERSION=$(printf "%02d" $RELEASE_VERSION)
-			fi
-		fi
-	done
+    if [ ! -f "$ini_file" ]; then
+        echo "[ERROR] File $ini_file not found!"
+        return 1
+    fi
 
-	echo "RELEASE_VERSION = $RELEASE_VERSION"
-	popd 2>&1 > /dev/null
+    echo "[INFO] Updating OEMInfo.ini ..."
+    echo "[INFO] Build_Date: $DATE"
+    echo "[INFO] Image_Version: v${RELEASE_VERSION}"
 
-	IMAGE_VER="${PROJECT}_${OS_BSP}${DISTRO}${RELEASE_VERSION}_${KERNEL_VERSION}_${CHIP_NAME}_${RAM_SIZE}_${DATE}"
-	UFS_IMAGE_VER="${PROJECT}_${OS_BSP}${DISTRO}${RELEASE_VERSION}_${KERNEL_VERSION}_${CHIP_NAME}_${RAM_SIZE}_${STORAGE}_${DATE}"
-	EMMC_IMAGE_VER="${PROJECT}_${OS_BSP}${DISTRO}${RELEASE_VERSION}_${KERNEL_VERSION}_${CHIP_NAME}_${RAM_SIZE}_emmc_${DATE}"
-}
+    # 更新 Build_Date
+    sed -i "s/^Build_Date:.*/Build_Date: $DATE/" "$ini_file"
+    # 更新 Image_Version
+    sed -i "s/^Image_Version:.*/Image_Version: v${RELEASE_VERSION}/" "$ini_file"
 
-function add_version()
-{
-	# Set Linux version
-	echo "[ADV] add linux version"
-	OFFICIAL_VER="${OS_BSP}${DISTRO}${RELEASE_VERSION}"
-	sed -i "/LINUX_VERSION_EXTENSION =/d" $ROOT_DIR/$KERNEL_PATH
-	echo "LINUX_VERSION_EXTENSION = \"-$OFFICIAL_VER\"" >> $ROOT_DIR/$KERNEL_PATH
-	cat $ROOT_DIR/$KERNEL_PATH
+    echo "[INFO] Done updating $ini_file."
 }
 
 function get_downloads()
@@ -199,7 +147,7 @@ OS,Qualcomm linux ${OS_BSP}${DISTRO}
 Part Number,N/A
 Author,
 Date,${DATE}
-Build Number,"${RELEASE_VERSION}"
+Build Number,"v${RELEASE_VERSION}"
 TAG,
 Tested Platform,${PRODUCT}
 MD5 Checksum,TGZ: ${MD5_SUM}
@@ -258,8 +206,7 @@ fi
 
 #prepare source code and build environment
 get_source_code
-get_release_version_and_set_image_version
-add_version
+update_oeminfo
 get_downloads
 set_environment
 build_image

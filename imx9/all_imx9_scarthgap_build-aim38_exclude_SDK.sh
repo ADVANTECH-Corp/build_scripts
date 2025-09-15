27766a4c5f1ae2ce98a71e6521388819eec564f2
Hank.Hsueh <Hank.Hsueh@advantech.com.tw>
2025-09-15 17:36:22 +0800

    [build][imx93] Adjust build script for ROM2820A1
    - Update all_imx9_scarthgap_build-aim38_exclude_SDK.sh
    - Change ROM2820A1 build option from '2G 1G' to '2G' only

--- imx9/all_imx9_scarthgap_build-aim38_exclude_SDK.sh
+++ imx9/all_imx9_scarthgap_build-aim38_exclude_SDK.sh
@@ -13,7 +13,7 @@ if [ $AOM5521A1 == true ]; then
 	[ "$?" -ne 0 ] && exit 1
 fi
 if [ $ROM2820A1 == true ]; then
-	$BUILD_SH rom2820a1-93 2820A1"$AIM_VERSION"LIV"$VERSION_NUM" "2G 1G" ""
+	$BUILD_SH rom2820a1-93 2820A1"$AIM_VERSION"LIV"$VERSION_NUM" "2G" ""
 	[ "$?" -ne 0 ] && exit 1
 fi
 echo "[ADV] All done!"

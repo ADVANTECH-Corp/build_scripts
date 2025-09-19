d6b868e38537f1b7d44dea25e9f4b8a867c76810
Alex19980122 <160805563+Alex19980122@users.noreply.github.com>
2025-09-19 10:37:40 +0800

    Update all_risc_nvidia_officialbuild.sh
    Add AIR030 project

--- nvidia/all_risc_nvidia_officialbuild.sh
+++ nvidia/all_risc_nvidia_officialbuild.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#AIR030A1_projects
+if [ "$air030a1" == "true" ]; then
+    PROJECT_BRANCH="air030"
+    export PROJECT_BRANCH
+    ./risc_nvidia_officialbuild.sh
+fi
+
 #EPCR7300A1_projects
 if [ "$epcr7300a1" == "true" ]; then
     PROJECT_BRANCH="epcr7300"

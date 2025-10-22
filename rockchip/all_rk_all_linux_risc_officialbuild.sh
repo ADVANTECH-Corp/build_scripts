3f86ecdbba86bcb914fe67bfbce974e62e5adac0
bo.xiao <bo.xiao@advantech.com>
2025-10-22 11:04:46 +0800

    add asra501a2 for build

--- rockchip/all_rk_all_linux_risc_officialbuild.sh
+++ rockchip/all_rk_all_linux_risc_officialbuild.sh
@@ -30,6 +30,19 @@ if [ "$ROM6881A1" == "true" ]; then
 	./rk_all_linux_risc_officialbuild.sh
 fi
 
+if [ "$ASRA501A2" == "true" ]; then
+	PROJECT_LIST="$PROJECT_LIST ASRA501"
+	PROJECT="asra501a2"
+	BOARD_CONFIG="adv_rk3588_asra501a2_defconfig"
+	RT_PATCH="true"
+
+	export PROJECT_LIST
+	export PROJECT
+	export HW_VER
+	export BOARD_CONFIG
+	export RT_PATCH
+	./rk_all_linux_risc_officialbuild.sh
+fi
 
 #rk3576_projects
 if [ "$AOM3841A1" == "true" ]; then

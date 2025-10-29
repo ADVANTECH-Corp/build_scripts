6c0d92ab30be728259419cb391b3c92c2f5364a5
wei.zeng <wei.zeng@advantech.com.cn>
2025-10-29 12:06:43 +0800

    [android15] added support file

---
+++ rockchip/all_rk_android_V15_officialbuild.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+MACHINE_LIST=""
+
+#rk3576_projects
+if [ "$AOM3841A1" == "true" ]; then
+        UBOOT_DEFCONFIG=rk3576_aom3841a1
+        KERNEL_DEFCONFIG=rk3576_adv_defconfig
+        KERNEL_DTB=rk3576-aom3841-a1.img
+        ANDROID_PRODUCT=aom3841_u-userdebug
+        MACHINE_LIST="$MACHINE_LIST aom3841"
+
+        export UBOOT_DEFCONFIG
+        export KERNEL_DEFCONFIG
+        export KERNEL_DTB
+        export ANDROID_PRODUCT
+        export MACHINE_LIST
+        ./rk_android_V15_officialbuild.sh
+fi

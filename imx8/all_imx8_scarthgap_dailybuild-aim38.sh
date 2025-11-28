101821175ac1773087db2b729f197668f3b4e585
Winston.Huang <winston.huang@advantech.com.tw>
2025-11-28 13:43:02 +0800

    [Yocto5.0] Add ROM5720A1 2G in dailybuild; officialbuild added on 2024/12/30 by Hank.Hseuh

--- imx8/all_imx8_scarthgap_dailybuild-aim38.sh
+++ imx8/all_imx8_scarthgap_dailybuild-aim38.sh
@@ -28,6 +28,11 @@ if [ $ROM2620A1 == true ]; then
 	[ "$?" -ne 0 ] && exit 1
 fi
 
+if [ $ROM5720A1 == true ]; then
+        $BUILD_SH rom5720a1-8M 5720A1"$AIM_VERSION"LIV"$VERSION_NUM" "2G" ""
+        [ "$?" -ne 0 ] && exit 1
+fi
+
 if [ $ROM5721A1 == true ]; then
         $BUILD_SH rom5721a1-8MM 5721A1"$AIM_VERSION"LIV"$VERSION_NUM" "2G" ""
         [ "$?" -ne 0 ] && exit 1

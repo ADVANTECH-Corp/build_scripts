4407f082612a987f8f007ae524695442e8288903
wei.zeng <wei.zeng@advantech.com.cn>
2026-03-24 16:31:41 +0800

    [rk3399] debian10 support 4710lite

--- rockchip/all_rk3399_debian9_v231_risc_officialbuild.sh
+++ rockchip/all_rk3399_debian9_v231_risc_officialbuild.sh
@@ -43,6 +43,23 @@ if [ "$RSB4710A2" == "true" ]; then
 	./rk3399_debian9_v231_risc_officialbuild.sh $VERSION_NUM
 fi
 
+if [ "$RSB4710A2LITE" == "true" ]; then
+        UBOOT_DEFCONFIG=rk3399_rsb4710a2_lite_2G
+        KERNEL_DEFCONFIG=rk3399_adv_defconfig
+        KERNEL_DTB=rk3399-rsb4710-a2-lite.img
+        MACHINE_LIST="$MACHINE_LIST RSB4710lite"
+        MODEL_NAME="RSB4710lite"
+        HW_VER="A2"
+
+        export UBOOT_DEFCONFIG
+        export KERNEL_DEFCONFIG
+        export KERNEL_DTB
+        export MACHINE_LIST
+        export MODEL_NAME
+        export HW_VER
+        ./rk3399_debian9_v231_risc_officialbuild.sh $VERSION_NUM
+fi
+
 if [ "$ROM5780A3" == "true" ]; then
 	UBOOT_DEFCONFIG=rk3399_rom5780a3_2G
 	KERNEL_DEFCONFIG=rk3399_adv_defconfig
